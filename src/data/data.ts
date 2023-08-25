112fe5e9348e0e3d90ec723c05d63bec4c3fdee2
Donghun kim <59818904+olennis@users.noreply.github.com>
2023-08-25 21:50:39 +0900

    feat : Tag 컴포넌트 구현 (#20)

--- src/data/data.ts
+++ src/data/data.ts
@@ -12,6 +12,7 @@ export interface Data {
   icon: string;
   isPersonal: boolean;
   company: string;
+  tags: string[];
 }
 
 export const PROJECT_DATA: Data[] = [
@@ -19,52 +20,57 @@ export const PROJECT_DATA: Data[] = [
     id: 0,
     title: "URCLASS",
     duration: "21.12 ~ 22.08",
+    icon: "/images/codestates.png",
+    isPersonal: false,
+    company: "codestates",
+    tags: ["typescript", "react", "apollo", "graphQL", "styled-component"],
     description: `#### 세부내용
 
     > todo: React component preview markdown text.`,
     content: "유어클래스.gif url",
-    icon: "/images/codestates.png",
-    isPersonal: false,
-    company: "codestates",
   },
   {
     id: 1,
     title: "COZ 어드민",
     duration: "21.12 ~ 22.08",
-    description: "유어클래스 설명",
-    content: "유어클래스.gif url",
     icon: "/images/urclass-admin.png",
     isPersonal: false,
     company: "codestates",
+    tags: ["typescript", "react", "apollo", "graphQL", "styled-component"],
+    description: "유어클래스 설명",
+    content: "유어클래스.gif url",
   },
   {
     id: 2,
     title: "BOWL",
     duration: "22.08 ~ 23.08",
-    description: "bowl 설명",
-    content: "bowl.gif url",
     icon: "/images/bowl.png",
     isPersonal: false,
     company: "codestates",
+    tags: ["typescript", "nextJS", "react-query", "emotion"],
+    description: "bowl 설명",
+    content: "bowl.gif url",
   },
   {
     id: 3,
     title: "BOWL 어드민",
     duration: "22.08 ~ 23.08",
-    description: "bowl 어드민 설명",
-    content: "bowl 어드민.gif url",
     icon: "/images/bowl-admin.png",
     isPersonal: false,
     company: "codestates",
+    tags: ["typescript", "nextJS", "react-query", "emotion"],
+    description: "bowl 어드민 설명",
+    content: "bowl 어드민.gif url",
   },
   {
     id: 4,
     title: "DKT",
     duration: "21.05 ~ 21.12",
-    description: "디케이테크인 프로젝트 설명",
-    content: "디케이테크인.gif url",
     icon: "/images/dktechin.png",
     isPersonal: false,
     company: "dk techin",
+    tags: ["vue", "nuxt", "jQuery", "ejs"],
+    description: "디케이테크인 프로젝트 설명",
+    content: "디케이테크인.gif url",
   },
 ];
