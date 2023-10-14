43c3e566a5319682b823f347ecec4ac3b9bb5d88
Donghun kim <kisses217@gmail.com>
2023-10-14 20:42:29 +0900

    Updates

--- src/components/Tag/index.tsx
+++
@@ -1,9 +0,0 @@
-import { TagWrapper } from "./styled";
-
-interface TagProps {
-  text: string;
-}
-
-export const Tag = ({ text }: TagProps) => {
-  return <TagWrapper>{text}</TagWrapper>;
-};
--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./Dock";
-export * from "./Header";
-export * from "./Intro";
-export * from "./Stack";
-export * from "./Window";
